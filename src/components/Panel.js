3aa0cc60c1629c77596f89e19002f944992f0abc
sagar-savaliya <sagarsavaliya407@gmail.com>
2018-06-20 16:01:25 +0530

    constant added

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,16 +2,17 @@ import React from "react";
 import ActiveToDoText from '../containers/ActiveToDoText'
 import ClearCompletedButton from '../containers/ClearCompletedButton'
 import ModeButton from '../containers/ModeButton';
+import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters'
 
 function Panel(){
     return(
         <div className='todo__pannel'>
             <ActiveToDoText />
-            <ModeButton mode="SHOW_ALL"
+            <ModeButton mode={SHOW_ALL}
                         text="All" />
-            <ModeButton mode="SHOW_ACTIVE"
+            <ModeButton mode={SHOW_ACTIVE}
                         text="Active" />
-            <ModeButton mode="SHOW_COMPLETED"
+            <ModeButton mode={SHOW_COMPLETED}
                         text="Completed" />
             <ClearCompletedButton />
         </div>
